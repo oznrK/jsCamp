0039563727f7882443cabb1e99dde377ea70d5ef
oznur <oznur@DESKTOP-QHHBNDV>
2021-10-25 13:45:43 +0300

    JS 1.ders

---
+++ src/app.js
@@ -0,0 +1,29 @@
+console.log("merhaba kodlama.io")
+//JS type safe değildir. 
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+console.log(euroDun)
+
+//array
+//camelCasing - PascalCasing
+let konutKredileri = ["konut kredisi", "emlak konut kredisi", "kamu konut kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+
+}
+console.log("/<ul>")
+
+console.log(konutKredileri)
+
